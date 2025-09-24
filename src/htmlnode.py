d5db94418eb43220380c2025cff6f7f5c8ccc8f1
reyhega <reyhega@gmail.com>
2025-09-24 16:21:35 -0600

    CH 2 - L3: Add test cases

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,8 +4,8 @@ class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
-        self.children = children if children is not None else []
-        self.props = props if props is not None else {}
+        self.children = children
+        self.props = props
         
     def to_html(self):
         raise NotImplementedError
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,19 +4,30 @@ from htmlnode import *
 
 class TestHTMLNode(unittest.TestCase):
     def test_multiple_props(self):
-        node = HTMLNode("a", "Testing multiple props", [], {"href": "https://www.boot.dev", "target": "_self"})
+        node = HTMLNode("a", "Testing multiple props", None, {"href": "https://www.boot.dev", "target": "_self"})
         node_result = ' href="https://www.boot.dev" target="_self"'
         self.assertEqual(node.props_to_html(), node_result)
 
     def test_single_prop(self):
-        node = HTMLNode("img", None, [], {"src": "single_prop.jpg"})
+        node = HTMLNode("img", None, None, {"src": "single_prop.jpg"})
         node_result = ' src="single_prop.jpg"'
         self.assertEqual(node.props_to_html(), node_result)
 
     def test_empty_props(self):
-        node = HTMLNode("p", "Empty props test", [], {})
+        node = HTMLNode("p", "Empty props test", None, None)
         node_result = ""
         self.assertEqual(node.props_to_html(), node_result)
+
+    def test_values(self):
+        node = HTMLNode("div", "Test, test",)
+        self.assertEqual(node.tag, "div")
+        self.assertEqual(node.value, "Test, test")
+        self.assertEqual(node.children, None)
+        self.assertEqual(node.props, None)
+
+    def test_repr(self):
+        node = HTMLNode("p", "This should display...", None, {"class": "deez"})
+        self.assertEqual(node.__repr__(),"HTMLNode(tag=p, value=This should display..., children=None, props={'class': 'deez'})")
 
 
 if __name__ == "__main__":
