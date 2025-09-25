c89f451b614149a99d0fb12b183d0c495c7c23c4
reyhega <reyhega@gmail.com>
2025-09-24 20:26:14 -0600

    CH 2 - L6: TextNode to HTML Node & test cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,9 +1,9 @@
 import unittest
 
-from textnode import TextNode, TextType
-
+from textnode import TextType, TextNode, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
+
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
@@ -18,6 +18,36 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is an image", TextType.IMAGE, "linktoimage.png")
         node2 = TextNode("This is a link", TextType.LINK, "https://www.boot.dev")
         self.assertNotEqual(node, node2)
+
+
+class TextTextNodeToHTMLNode(unittest.TestCase):
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_img(self):
+        node = TextNode("this is an image", TextType.IMAGE, "https://www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.google.com", "alt": "this is an image"},
+        )
+
+    def test_link(self):
+        node = TextNode("this is a link", TextType.LINK, "https://www.google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "this is a link")
+        self.assertEqual(
+            html_node.props,
+            {"href": "https://www.google.com"},
+        
+        )
 
 
 if __name__ == "__main__":
