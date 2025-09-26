76af1294812a075350c172096a09fc75ce38ae19
reyhega <reyhega@gmail.com>
2025-09-26 03:20:16 -0600

    CH 3 - L6: Text to TextNodes

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,13 @@
 import unittest
 
-from inline_markdown import (split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link)
+from inline_markdown import (
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links, 
+    split_nodes_image,
+    split_nodes_link,
+    text_to_textnodes,
+    )
 
 from textnode import TextNode, TextType
 
@@ -122,6 +129,27 @@ class TestSplitDelimeter(unittest.TestCase):
             ],
             new_nodes,
         )
+
+################# Text to TextNode Tests #################
+
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        nodes_result = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(nodes, nodes_result)
+
+
 
 if __name__ == "__main__":
     unittest.main()
